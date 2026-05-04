7dcc4060521847a614496fccbec7debf40cc2e63
tienditamadrelucia <tienditamadrelucia@gmail.com>
2026-05-03 23:13:24 -0400

    toma de inventario 15

--- models/Entrada.js
+++ models/Entrada.js
@@ -3,17 +3,19 @@ import mongoose from "mongoose";
 
 const entradaSchema = new mongoose.Schema(
   {
-    fecha: {type: Date, required: true},
-    categoria: {type: String, required: true},
-    productoId: {type: mongoose.Schema.Types.ObjectId,
-      ref: "Producto",
-      required: true
+      fecha: { type: Date, required: true },
+      // Código de la categoría (ALB, VEL, etc.)
+      categoria: { type: String },
+      // ID del producto
+      productoId: { type: mongoose.Schema.Types.ObjectId, ref: "Producto", required: true },
+      // Código del producto (opcional, porque ya tenemos el ID)
+      codigo: { type: Number },
+      // Cantidad que sale
+      cantidad: { type: Number, required: true },
+      // Motivo de la salida: VENTA, AJUSTE, etc.
+      observacion: { type: String, required: true }
     },
-    codigo: {type: Number, required: true},
-    cantidad: {type: Number, required: true },
-    observacion: {type: String, required: true }
-  },
-  { timestamps: true }
-);
+    { timestamps: true }
+  );
 
 export default mongoose.model("Entrada", entradaSchema);
--- models/dbSalidas.js
+++ models/dbSalidas.js
@@ -4,19 +4,14 @@ import mongoose from "mongoose";
 const salidaSchema = new mongoose.Schema(
   {
     fecha: { type: Date, required: true },
-
     // Código de la categoría (ALB, VEL, etc.)
     categoria: { type: String },
-
     // ID del producto
     productoId: { type: mongoose.Schema.Types.ObjectId, ref: "Producto", required: true },
-
     // Código del producto (opcional, porque ya tenemos el ID)
     codigo: { type: Number },
-
     // Cantidad que sale
     cantidad: { type: Number, required: true },
-
     // Motivo de la salida: VENTA, AJUSTE, etc.
     observacion: { type: String, required: true }
   },
